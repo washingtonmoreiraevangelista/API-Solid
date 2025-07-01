63bfa8742e677f79ae438f9310b05a98314967a7
washington <washingtonmoreiraevangelista@gmail.com>
2025-07-01 14:52:53 -0300

    feat: setup project structure with Prisma and Fastify

---
+++ env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config' 
+import { z } from 'zod'
+
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+// validação do ambiente
+const _env = envSchema.safeParse(process.env)
+
+// se a validação falhar, exibe o erro
+if(_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+
+  // encerra o processo e nao inicia o servidor
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
